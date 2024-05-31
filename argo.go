7a932b1a3e4f41b7953929e3c5cf9f84074f994e
joelmce <joelanthony.mac@gmail.com>
2024-06-01 01:28:50 +1000

    Finish parse function

--- argo.go
+++ argo.go
@@ -76,6 +76,28 @@ func trimWhitespaces(value string) string {
 	return strings.Trim(value, " ")
 }
 
+func formatCommandValues(values []string) (formatted []string) {
+
+	formatted = make([]string, 0)
+
+	// split a value by `=`
+	for _, value := range values {
+		if isFlag(value) {
+			parts := strings.Split(value, "=")
+
+			for _, part := range parts {
+				if strings.Trim(part, " ") != "" {
+					formatted = append(formatted, part)
+				}
+			}
+		} else {
+			formatted = append(formatted, value)
+		}
+	}
+
+	return
+}
+
 // Validate if the command is a flag or not
 func isFlag(value string) bool {
 	return len(value) >= 2 && len(value) == 2 && !strings.HasPrefix(value, "-")
@@ -146,6 +168,13 @@ func (e ErrorUnsupportedFlag) Error() string {
 	return fmt.Sprintf("unsupported flag %s found in the arguments", e.Name)
 }
 
+func NewRegistry() Registry {
+	return make(Registry)
+}
+
+/*
+Register will create a new Registry for a base command.
+*/
 func (r Registry) Register(name string, version string, desc string) (*CommandRegistry, bool) {
 	commandName := removeWhitespaces(name)
 
@@ -165,17 +194,184 @@ func (r Registry) Register(name string, version string, desc string) (*CommandRe
 	return commandConfig, false
 }
 
-func (r Registry) Parse(args []string) (*CommandRegistry, error) {
-	var commandName string
+/*
+So once the user has registered a command we can start
+appending them to the registry
+*/
+func (cr CommandRegistry) AddCommand(cmd *Command) Command {
+	cr.Commands[cmd.Name] = cmd
+	return *cr.Commands[cmd.Name]
+}
 
+func (cmd *Command) AddArg(arg *Arg) (*Arg, bool) {
+	// Sanitise inputs
+	cleanName := removeWhitespaces(arg.Name)
+	cleanDefault := removeWhitespaces(arg.Default)
+
+	variadic := false
+	if ok, argName := isVariadicArg(cleanName); ok {
+		cleanName = argName
+		variadic = true
+	}
+
+	if _arg, ok := cmd.Args[cleanName]; ok {
+		return _arg, true
+	}
+
+	newArg := &Arg{
+		Name:       cleanName,
+		IsVariadic: variadic,
+		Default:    cleanDefault,
+	}
+
+	cmd.Args[cleanName] = newArg
+	cmd.ArgNames = append(cmd.ArgNames, cleanName)
+
+	return arg, false
+}
+
+func (cmd *Command) AddFlag(flg *Flag) (*Flag, bool) {
+	// Sanitise inputs
+	cleanName := removeWhitespaces(flg.Name)
+	cleanShortName := removeWhitespaces(flg.ShortName)
+	cleanDefaultValue := removeWhitespaces(flg.Default)
+
+	isInverted := false
+
+	if cleanShortName != "" {
+		cleanShortName = cleanShortName[:1]
+	}
+
+	if flg.IsBoolean {
+		if strings.HasPrefix(flg.Name, "no-") {
+			isInverted = true
+			cleanName = strings.TrimLeft(flg.Name, "no-")
+			cleanDefaultValue = "true"
+			cleanShortName = ""
+		} else {
+			cleanDefaultValue = "false"
+		}
+	}
+
+	if _flag, ok := cmd.Flags[cleanName]; ok {
+		return _flag, true
+	}
+
+	flag := &Flag{
+		Name:       cleanName,
+		ShortName:  cleanShortName,
+		IsBoolean:  flg.IsBoolean,
+		IsInverted: isInverted,
+		Default:    cleanDefaultValue,
+	}
+
+	cmd.Flags[cleanName] = flag
+
+	if len(cleanShortName) > 0 {
+		cmd.shorthandFlags[cleanShortName] = cleanName
+	}
+
+	return flag, false
+
+}
+
+func next(slice []string) (val string, newSlice []string) {
+	if len(slice) == 0 {
+		val, newSlice = "", make([]string, 0)
+		return
+	}
+
+	val = slice[0]
+
+	if len(slice) > 1 {
+		newSlice = slice[1:]
+	} else {
+		newSlice = make([]string, 0)
+	}
+
+	return
+}
+
+func (cmd *CommandRegistry) Parse(args []string) (*Command, error) {
+	// Root command should sit at position 0.
+	// Maybe there's a better way to do this?
+	var commandName string = args[0]
+
+	formattedArguments := formatCommandValues(args)
+
+	// Return ErrorUnsupportedFlag if the flag is unsupported.
 	for _, val := range args {
 		if isFlag(val) && isUnsupportedFlag(val) {
 			return nil, ErrorUnsupportedFlag{val}
 		}
 	}
 
-	// if command is not registered, return `ErrorUnknownCommand` error
-	if _, ok := r[commandName]; !ok {
-		return nil, ErrorUnknownCommand{commandName}
+	commands := cmd.Commands[commandName]
+
+	for {
+		var value string
+		value, valuesToParse := next(formattedArguments)
+
+		if len(value) == 0 {
+			break
+		}
+
+		if isFlag(value) {
+			name := strings.TrimLeft(value, "-")
+			var flag *Flag
+
+			if isShortFlag(value) {
+				if _, ok := commands.shorthandFlags[name]; !ok {
+					return nil, ErrorUnknownFlag{value}
+				}
+			} else {
+				if ok, flagName := isInvertedFlag(value); ok {
+					if _, ok := commands.Flags[flagName]; !ok {
+						return nil, ErrorUnknownFlag{value}
+					}
+
+					flag = commands.Flags[flagName]
+				} else {
+					if _flag, ok := commands.Flags[flagName]; !ok || _flag.IsInverted {
+						return nil, ErrorUnknownFlag{value}
+					}
+
+					flag = commands.Flags[name]
+				}
+			}
+
+			if flag.IsBoolean {
+				if flag.IsInverted {
+					flag.Value = "false"
+				} else {
+					flag.Value = "true"
+				}
+			} else {
+				if nextValue, nextValuesToProcess := next(valuesToParse); len(nextValue) != 0 && !isFlag(nextValue) {
+					flag.Value = nextValue
+					valuesToParse = nextValuesToProcess
+				}
+			}
+		} else {
+			// process as argument
+			for index, argName := range commands.ArgNames {
+
+				// get argument object stored in the `commandConfig`
+				arg := commands.Args[argName]
+
+				// assign value if value of the argument is empty
+				if len(arg.Value) == 0 {
+					arg.Value = value
+					break
+				}
+
+				// if last argument is a variadic argument, append values
+				if (index == len(commands.ArgNames)-1) && arg.IsVariadic {
+					arg.Value += fmt.Sprintf(",%s", value)
+				}
+			}
+		}
 	}
+
+	return commands, nil
 }
